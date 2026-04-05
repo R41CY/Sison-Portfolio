f88833470e0e782e05eddfcd67c25521987ea344
R41CY <raicyfoej@gmail.com>
2026-04-05 22:12:00 +0800

    adjust volume

--- script.js
+++ script.js
@@ -370,7 +370,7 @@
   /* Base Audio Object Context Handler */
   var audio = document.getElementById('bg-music');
   var musicBtn = document.getElementById('music-btn');
-  audio.volume = 0.005;
+  audio.volume = 0.010;
 
   function setPlaying(on) { musicPanel.classList.toggle('playing', on); }
   function playMusic() { audio.play().then(function () { setPlaying(true); }).catch(function () { setPlaying(false); }); }
